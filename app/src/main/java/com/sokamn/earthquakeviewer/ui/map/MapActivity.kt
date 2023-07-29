a7d08b082e6c705828fad7ec25d8f4411ebccbcd
Matias Alzú <102830508+Sokamn@users.noreply.github.com>
2023-07-29 15:24:57 -0300

    update Fisrt Launch 15:24 29/7

---
+++ app/src/main/java/com/sokamn/earthquakeviewer/ui/map/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.sokamn.earthquakeviewer.ui.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sokamn.earthquakeviewer.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
